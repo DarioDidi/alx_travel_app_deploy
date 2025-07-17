460833ac9082f21ba4a66a469147887782c43a33
Dario Didi <dididario.dd2@gmail.com>
2025-07-18 02:45:23 +0300

    add readme ,relocate serializers for testing

---
+++ alx_travel_app/alx_travel_app/README.md
@@ -0,0 +1,1 @@
+A booking site with features and functionalities including user authentication, property management, booking system, and payments.
---
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -0,0 +1,64 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from .models import Booking, Listing, Review, Payment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", "email", "role"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "id": {"read_only": True},
+        }
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            role=validated_data.get("role", "guest"),
+        )
+        return user
+
+
+class ListinSerializer(serializers.ModelSerializer):
+    host = UserSerializer(read_only=True)
+
+    class meta:
+        model = Listing
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+class BookingSerializer(serializers.Serializer):
+    property = ListinSerializer(read_only=True)
+    customer = UserSerializer(read_only=True)
+
+    class meta:
+        model = Booking
+        fields = "__all__"
+        extra_kwargs = {"id": {"read_only": True}}
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Review
+        field = "__all__"
+        extra_kwargs = {
+            "property": {"write_only": True},
+            "id": {"read_only": True},
+        }
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+        extra_kwargs = {
+            "booking": {"write_only": True},
+            "id": {"read_only": True},
+        }
